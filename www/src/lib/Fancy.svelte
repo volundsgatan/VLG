e3a2d29fa6cbf72066103d17f0129284e146ed12
Gustav Westling <gustav@getsturdy.com>
2022-08-30 23:30:48 +0200

    www/fancy: add sensors

--- www/src/lib/Fancy.svelte
+++ www/src/lib/Fancy.svelte
@@ -3,6 +3,7 @@
 	import { type State as SonosState, type Zone } from './sonosTypes';
 	import { groups, type State } from './devices';
 	import NowPlaying from '$lib/music/NowPlaying.svelte';
+	import Device from '$lib/device/Device.svelte';
 
 	export let states: Record<string, State> = {};
 	export let ws: WebSocket;
@@ -58,29 +59,72 @@
 		}
 	};
 
+	const getState = (states, addr: string) => {
+		return Object.values(states).find((s) => s.device?.ieeeAddr === addr);
+	};
+
 	$: bg = getBg(states);
 </script>
 
-<div
-	style="background-image: url('{bg}')"
-	class="h-[576px] w-[1024px] bg-[length:1024px_576px] bg-no-repeat text-white	"
->
-	<div
-		on:click={() => toggleLights('Bedroom')}
-		class="absolute top-[100px]  left-[60px] h-[200px] w-[310px] cursor-pointer"
-	/>
-	<div
-		on:click={() => toggleLights('Kitchen')}
-		class="absolute top-[310px]  left-[60px] h-[160px] w-[330px] cursor-pointer"
-	/>
-	<div
-		on:click={() => toggleLights('Entrance')}
-		class="absolute top-[230px]   left-[400px] h-[120px] w-[200px] cursor-pointer"
-	/>
-	<div
-		on:click={() => toggleLights('Living Room')}
-		class="absolute  top-[100px]  left-[630px] h-[200px] w-[320px] cursor-pointer"
-	/>
-</div>
+<svelte:head>
+	<link rel="preload" as="image" href="/floorplan_0000.png" />
+	<link rel="preload" as="image" href="/floorplan_0001.png" />
+	<link rel="preload" as="image" href="/floorplan_0010.png" />
+	<link rel="preload" as="image" href="/floorplan_0011.png" />
+	<link rel="preload" as="image" href="/floorplan_0100.png" />
+	<link rel="preload" as="image" href="/floorplan_0101.png" />
+	<link rel="preload" as="image" href="/floorplan_0110.png" />
+	<link rel="preload" as="image" href="/floorplan_0111.png" />
+	<link rel="preload" as="image" href="/floorplan_1000.png" />
+	<link rel="preload" as="image" href="/floorplan_1001.png" />
+	<link rel="preload" as="image" href="/floorplan_1010.png" />
+	<link rel="preload" as="image" href="/floorplan_1011.png" />
+	<link rel="preload" as="image" href="/floorplan_1100.png" />
+	<link rel="preload" as="image" href="/floorplan_1101.png" />
+	<link rel="preload" as="image" href="/floorplan_1110.png" />
+	<link rel="preload" as="image" href="/floorplan_1111.png" />
+</svelte:head>
 
-<NowPlaying zones={sonosZones} on:sonosUpdated />
+<div class="flex h-full flex-col justify-between space-y-2 bg-gray-300">
+	<div
+		style="background-image: url('{bg}')"
+		class="-mt-16 h-[576px] w-[1024px] bg-[length:1024px_576px] bg-no-repeat text-white transition-all duration-500	"
+	>
+		<div
+			on:click={() => toggleLights('Bedroom')}
+			class="absolute  top-[50px]  left-[60px] h-[200px] w-[310px] cursor-pointer"
+		/>
+		<div
+			on:click={() => toggleLights('Kitchen')}
+			class="absolute  top-[260px]  left-[60px] h-[160px] w-[330px] cursor-pointer"
+		/>
+		<div
+			on:click={() => toggleLights('Entrance')}
+			class="absolute  top-[160px]   left-[400px] h-[120px] w-[200px] cursor-pointer"
+		/>
+		<div
+			on:click={() => toggleLights('Living Room')}
+			class="absolute   top-[50px]  left-[630px] h-[200px] w-[320px] cursor-pointer"
+		/>
+
+		<!-- Door Sensor -->
+		<Device
+			class="absolute top-[400px] left-[350px] text-black"
+			state={getState(states, '0x00158d000839a1f9')}
+		/>
+		<!-- Frigde -->
+		<Device
+			class="absolute top-[375px] left-[235px] text-black"
+			state={getState(states, '0x00158d0007f82457')}
+		/>
+		<!-- Window Sensor -->
+		<Device
+			class="absolute top-[130px] left-[945px] text-black"
+			state={getState(states, '0x00158d0008399e95')}
+		/>
+	</div>
+
+	<div class="p-2">
+		<NowPlaying zones={sonosZones} on:sonosUpdated />
+	</div>
+</div>
---
+++ www/src/lib/device/Device.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let className;
+	export let state;
+	export { className as class };
+	import Contact from './Contact.svelte';
+	import Temperature from './Temperature.svelte';
+	import Occupancy from './Occupancy.svelte';
+</script>
+
+<div class={className}>
+	{#if !state}
+		???
+	{:else if state?.temperature}
+		<Temperature temperature={state.temperature} light={true} />
+	{:else if state.contact !== undefined}
+		<Contact contact={state.contact} />
+	{:else if state.occupancy !== undefined}
+		<Occupancy occupancy={state.occupancy} />
+	{/if}
+</div>
