fbca73002ec71de6617fce4668d79bd627d5eb54
Gustav Westling <gustav@westling.dev>
2023-01-01 13:54:20 +0100

    dashboard: hide effects if none availble

--- www/src/lib/dashboard/Effect.svelte
+++ www/src/lib/dashboard/Effect.svelte
@@ -34,13 +34,15 @@
 	};
 </script>
 
-<div class="font-medium capitalize">
-	{name}
-</div>
-<div class="flex items-center gap-2">
-	{#each effectsWithIcons as e}
-		<div on:click={() => set(e.name)} class="cursor-pointer">
-			{e.icon}
-		</div>
-	{/each}
-</div>
+{#if effectsWithIcons.length > 0}
+	<div class="font-medium capitalize">
+		{name}
+	</div>
+	<div class="flex items-center gap-2">
+		{#each effectsWithIcons as e}
+			<div on:click={() => set(e.name)} class="cursor-pointer">
+				{e.icon}
+			</div>
+		{/each}
+	</div>
+{/if}
