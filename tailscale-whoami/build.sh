577d11645848908343d0b0d11ac0c16dcacbcc94
Gustav Westling <gustav@westling.dev>
2023-03-28 22:23:19 +0200

    action: test

--- tailscale-whoami/build.sh
+++ tailscale-whoami/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker buildx build --platform linux/arm64,linux/amd64 -t registry.unicorn-alligator.ts.net/whoami:latest . --push
+#podman buildx build --platform linux/arm64,linux/amd64 -t registry.unicorn-alligator.ts.net/whoami:latest .
+
+buildah build -f Containerfile --jobs=4 --platform=linux/amd64,linux/arm64 --manifest registry.unicorn-alligator.ts.net/whoami:latest .
