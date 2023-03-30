c1a93443b247ac556bdb653f5288f6431b7494f0
Gustav Westling <gustav@polar.sh>
2023-03-30 16:40:41 +0200

    test

--- tailscale-whoami/build.sh
+++ tailscale-whoami/build.sh
@@ -2,4 +2,9 @@
 
 #podman buildx build --platform linux/arm64,linux/amd64 -t registry.unicorn-alligator.ts.net/whoami:latest .
 
-buildah build -f Containerfile --jobs=4 --platform=linux/amd64,linux/arm64 --manifest registry.unicorn-alligator.ts.net/whoami:latest .
+buildah build \
+    -f Containerfile \
+    --jobs=4 \
+    --storage-driver vfs \
+    --platform=linux/amd64,linux/arm64 \
+    --manifest registry.unicorn-alligator.ts.net/whoami:latest .
