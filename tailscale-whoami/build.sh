020330160e0244e3f2e0ca581f521dc82d6e517c
Gustav Westling <gustav@polar.sh>
2023-04-05 17:15:57 +0200

    ci: push

--- tailscale-whoami/build.sh
+++ tailscale-whoami/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -f Containerfile -t registry.unicorn-alligator.ts.net/whoami:latest .
-docker push registry.unicorn-alligator.ts.net/whoami:latest
+docker build -f Containerfile -t registry.registry.svc.cluster.local/whoami:latest .
+docker push registry.registry.svc.cluster.local/whoami:latest
+
+#docker build -f Containerfile -t registry.unicorn-alligator.ts.net/whoami:latest .
+#docker push registry.unicorn-alligator.ts.net/whoami:latest
