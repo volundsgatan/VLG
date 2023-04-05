79ac98cdcdbbdc04e05703ca6d157979ce3600c9
Gustav Westling <gustav@polar.sh>
2023-04-05 17:29:17 +0200

    ci: push

--- tailscale-whoami/build.sh
+++ tailscale-whoami/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -f Containerfile -t registry.registry.svc.cluster.local:80/whoami:latest .
-DOCKER_OPTS="--insecure-registry=registry.registry.svc.cluster.local" docker push registry.registry.svc.cluster.local:80/whoami:latest
+DOCKER_OPTS="--insecure-registry=registry.registry.svc.cluster.local" docker push http://registry.registry.svc.cluster.local:80/whoami:latest
 
 #docker build -f Containerfile -t registry.unicorn-alligator.ts.net/whoami:latest .
 #docker push registry.unicorn-alligator.ts.net/whoami:latest
