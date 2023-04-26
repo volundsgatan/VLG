99394566839dcd272ca112ca4c40dd4ad4cfd77c
Gustav Westling <gustav@westling.dev>
2023-04-26 18:19:49 +0200

    www/weather: show sunrise/sunset

---
+++ www/src/lib/weather/Sunset.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let hour: string;
+	export let minute: string;
+</script>
+
+<div class="p-2 text-center inline-flex flex-col items-center">
+	<div class="text-sm text-white">{hour}:{minute}</div>
+	<img class="w-6 h-6" src="/yr_icons/01m.svg" />
+	<div class="text-md text-white">Sunset</div>
+</div>
--- www/src/lib/weather/Weather.svelte
+++ www/src/lib/weather/Weather.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import config from '$lib/config';
-
-	import { onMount, tick } from 'svelte';
+	import { onMount } from 'svelte';
 	import Forecast from './Forecast.svelte';
+	import Sunset from './Sunset.svelte';
+	import Sunrise from './Sunrise.svelte';
 
 	type Forecast = {
 		in_hours: number;
@@ -28,13 +29,16 @@
 
 	interface Metric {
 		coordinates: string;
-		in_hours: string;
+		in_hours?: string;
 		name: string;
 		code?: string;
 	}
 
 	let forecastAirTemperature: Result[];
 	let forecastInOneHourSymbol: Result[];
+
+	let sunriseSeconds: number;
+	let sunsetSeconds: number;
 
 	const ts_air_temperature = async () => {
 		const query = encodeURIComponent('yr_forecast_air_temperature{name="stockholm"}');
@@ -62,16 +66,38 @@
 		forecastInOneHourSymbol = root.data.result;
 	};
 
-	onMount(async () => {
+	const ts_sunrise = async () => {
+		const query = encodeURIComponent('yr_sunrise_sunrise_seconds_after_midnight{name="stockholm"}');
+		const response = await fetch(
+			`https://prometheus.${config.hostname}/api/v1/query?query=${query}`,
+			{ method: 'GET' }
+		);
+		const root = (await response.json()) as Root;
+		sunriseSeconds = parseInt(root.data.result[0].value[1]);
+	};
+
+	const ts_sunset = async () => {
+		const query = encodeURIComponent('yr_sunrise_sunset_seconds_after_midnight{name="stockholm"}');
+		const response = await fetch(
+			`https://prometheus.${config.hostname}/api/v1/query?query=${query}`,
+			{ method: 'GET' }
+		);
+		const root = (await response.json()) as Root;
+		sunsetSeconds = parseInt(root.data.result[0].value[1]);
+	};
+
+	const load = () => {
 		ts_air_temperature();
 		ts_in_one_hour_symbol();
+		ts_sunrise();
+		ts_sunset();
+	};
 
-		const interval_ts_air_temperature = setInterval(ts_air_temperature, 1000 * 60);
-		const interval_ts_in_one_hour_symbol = setInterval(ts_in_one_hour_symbol, 1000 * 60);
-
+	onMount(async () => {
+		load();
+		const interval = setInterval(load, 1000 * 60);
 		return () => {
-			clearInterval(interval_ts_air_temperature);
-			clearInterval(interval_ts_in_one_hour_symbol);
+			clearInterval(interval);
 		};
 	});
 
@@ -80,7 +106,7 @@
 			forecastInOneHourSymbol &&
 			forecastAirTemperature
 				.map((r): Forecast => {
-					const inHours = parseInt(r.metric.in_hours);
+					const inHours = parseInt(r.metric?.in_hours || '0');
 					const d = new Date();
 					const hour = (d.getHours() + inHours) % 24;
 					return {
@@ -95,12 +121,38 @@
 				.sort((a, b) => a.in_hours - b.in_hours)
 				.slice(0, 12)) ||
 		[];
+
+	const midnightSecondsHour = (sunriseSeconds: number) => {
+		const offset = new Date().getTimezoneOffset();
+		const d = new Date();
+		d.setHours(0, 0, sunriseSeconds);
+		const h = d.getHours() - offset / 60;
+		return h.toFixed(0).padStart(2, '0');
+	};
+
+	const midnightSecondsMinute = (sunriseSeconds: number) => {
+		const d = new Date();
+		d.setHours(0, 0, sunriseSeconds);
+		return d.getMinutes().toFixed(0).padStart(2, '0');
+	};
+
+	$: sunriseHour = midnightSecondsHour(sunriseSeconds);
+	$: sunriseMinute = midnightSecondsMinute(sunriseSeconds);
+
+	$: sunsetHour = midnightSecondsHour(sunsetSeconds);
+	$: sunsetMinute = midnightSecondsMinute(sunsetSeconds);
 </script>
 
 <div class="absolute top-[550px] z-10 left-[50px] bg-black/20 rounded-md">
 	<div class="flex divide-x divide-black/30">
 		{#each data as ts}
 			<Forecast hour={ts.hour} temp={ts.air_temperature} icon={ts.icon} />
+			{#if ts.hour === parseInt(sunsetHour)}
+				<Sunset hour={sunsetHour} minute={sunsetMinute} />
+			{/if}
+			{#if ts.hour === parseInt(sunriseHour)}
+				<Sunrise hour={sunriseHour} minute={sunriseMinute} />
+			{/if}
 		{/each}
 	</div>
 </div>
